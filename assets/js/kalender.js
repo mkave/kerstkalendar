1572773c8a601d4e3fb9bd24b86da52b0b927ba6
mkave <48988052+mkave@users.noreply.github.com>
2020-11-30 22:42:27 +0000

    Update kalender.js

--- assets/js/kalender.js
+++ assets/js/kalender.js
@@ -53,29 +53,24 @@ $('.card').click(function(){
 
 cardContent = [
     [
-        "We love you!",
-        "NNM06",
-        "https://thumbs.dreamstime.com/b/koala-bear-kid-cute-cartoon-koala-bear-characters-merry-christmas-vector-illustration-koala-christmas-lights-garland-166468607.jpg"
+        "Een verrassing speciaal voor jou!🎄",
+        "gY/c5rw",
+        "assets/img/d1.jpg"
     ],
-    [
-        "We have a million kisses for you!",
-        "Z8903", 
-        "https://i.pinimg.com/originals/03/35/d3/0335d30c746243c7ed4b9e1988e486b1.png"
-    ]
 ]
 
 naughtyContent = [
     /* ♡ */
     [
         "Kenny says stop trying to be sneaky or he's gonna put you on the naughty list 🐨📋",
-        "https://ctl.s6img.com/society6/img/MM1ac8gcZVFh7wjd_epSSZ7BT7o/w_700/prints/~artwork/s6-0082/a/32488466_8018428/~~/youre-the-reason-santa-even-has-a-naughty-list-prints.jpg"
+        "assets/img/kia1.jpg"
     ],
     [
         "Kenny says he knows what you're up to and he is not impressed 🐨🛑",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw4oPncMW2v0PWjLBgUgou2YJ2X6KeoWVWag&usqp=CAU"
+        "assets/img/kia2.jpg"
     ],
     [
         "Kenny's run the data and he says it isn't this date yet 🐨📅",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRw4oPncMW2v0PWjLBgUgou2YJ2X6KeoWVWag&usqp=CAU"
+        "assets/img/kia3.jpg"
     ],
 ]
